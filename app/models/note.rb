1db23499659ab60d2c0b07192ffb070cdacd9b63
selasiehanson <soundfever18@gmail.com>
2013-08-27 16:41:11 +0000

    refactored note and task model to use input class

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,7 +1,8 @@
-class Note < ActiveRecord::Base
-	attr_accessible :content
-  validates :content, presence: true
-  validates :project_id, presence: true
-  belongs_to :project
-  has_one :task
+class Note < Input
 end
+
+# attr_accessible :content
+# validates :content, presence: true
+# validates :project_id, presence: true
+# belongs_to :project
+# has_one :task
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,33 +1,13 @@
-class Task < ActiveRecord::Base
+class Task < Input
   
-  STATUS = { pending: 1,  started: 2, completed: 3  }.freeze
-
-  attr_accessible :status, :content, :note, :project
-
-  belongs_to :note
-  belongs_to :project
-
-  validates :content, presence: true
-  validates :note_id, presence: true
-  validates :status, :presence => true, :inclusion => { in: STATUS }
-  validates :project_id, presence: true
-
   scope :find_users_tasks_by_status, lambda{ |status, user, project|
   	{
   		:include => :project,
-      :order => "tasks.created_at DESC",
-  		:conditions => [ "tasks.status = :status AND projects.user_id = :user_id AND tasks.project_id = :project_id" , 
+      :order => "inputs.created_at DESC",
+  		:conditions => [ "status = :status AND projects.user_id = :user_id AND project_id = :project_id" , 
         { :status => Task::STATUS[status], :user_id => user.id , :project_id => project.id} ]
   	}
   }
-
-  def status
-  	STATUS.key(read_attribute(:status))
-  end
-
-  def status=(value)
-  	 write_attribute(:status, STATUS[value])
-  end
 
   def change_status()
     out = nil
@@ -39,4 +19,44 @@ class Task < ActiveRecord::Base
     end 
     # self.save
   end
+
+  # STATUS = { pending: 1,  started: 2, completed: 3  }.freeze
+
+  # attr_accessible :status, :content, :note, :project
+
+  # belongs_to :note
+  # belongs_to :project
+
+  # validates :content, presence: true
+  # validates :note_id, presence: true
+  # validates :status, :presence => true, :inclusion => { in: STATUS }
+  # validates :project_id, presence: true
+
+  # scope :find_users_tasks_by_status, lambda{ |status, user, project|
+  #   {
+  #     :include => :project,
+  #     :order => "tasks.created_at DESC",
+  #     :conditions => [ "tasks.status = :status AND projects.user_id = :user_id AND tasks.project_id = :project_id" , 
+  #       { :status => Task::STATUS[status], :user_id => user.id , :project_id => project.id} ]
+  #   }
+  # }
+
+  # def status
+  #   STATUS.key(read_attribute(:status))
+  # end
+
+  # def status=(value)
+  #    write_attribute(:status, STATUS[value])
+  # end
+
+  # def change_status()
+  #   out = nil
+  #   if self.status == :pending
+  #     self.status = :started
+  #   elsif self.status == :started
+  #     self.status = :completed
+  #   else
+  #   end 
+  #   # self.save
+  # end
 end
