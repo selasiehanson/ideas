56b92511092c24bf78397d13e511177f31afa649
selasiehanson <soundfever18@gmail.com>
2013-09-25 18:35:28 +0000

    added active links to pages and changed main page link from mystuff to projects as well removing the project categories link all together.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,8 @@ module ApplicationHelper
 	def pluralize_without_count(count, noun, text = nil)
 	  	count == 1 ? " #{noun}#{text}" : "#{noun.pluralize}#{text}"
 	end
+
+  def active_klass(in_page, out_page)
+   in_page.downcase == out_page.downcase ? "active" : ""
+  end
 end
