4947c82abe9d11dd7b7fb9c75987c3757acb2a3f
selasiehanson <soundfever18@gmail.com>
2014-04-08 00:34:16 +0000

    added auto close to message component

--- app/assets/angular/directives/message_component.js
+++ app/assets/angular/directives/message_component.js
@@ -1,6 +1,8 @@
 angular.module("messenger",[]).directive("axMsg", ["$rootScope", function ($rootScope){
 	var linkFn = function (scope, element, attrs){
 
+		var timer;
+		
 		$rootScope.$on('incomingMessage', function (event, args){
 				process(args)
 		});
@@ -17,15 +19,26 @@ angular.module("messenger",[]).directive("axMsg", ["$rootScope", function ($root
 				case "warn":
 					scope.type = "";
 				break;
-				default: 
+				default:
 					scope.type  = "alert-error"
 				break;
 			}
 			scope.display = ( args.show === true ) ? true : false
 			scope.message = args.message || "";
+			if(args.status == "success"){
+        if(timer){
+          clearTimeout(timer);
+        }
+
+        timer = setTimeout(function (){
+          scope.display = false;
+          scope.$apply();
+
+        }, 5000);
+      }
 		}
 	}
-	
+
 	return {
 		restrict: "E",
 		link : linkFn,
